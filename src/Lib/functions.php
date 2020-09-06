3da38863679bc89e997255caffd945c050f65f68
Manuel Prado <manuel@igeniuscr.com>
2020-09-06 08:44:54 -0600

    Add function myEach to fix deprecated each function

--- src/Lib/functions.php
+++ src/Lib/functions.php
@@ -13,7 +13,7 @@ if (!function_exists('__i18nInit')) {
      * Inits i18n multilanguage support.
      * cecr = Comprobante Electronico Costa Rica
      * @since 1.0.0
-     * 
+     *
      * @param string $locale Language locale to use.
      * @param string $cache  Cache file path.
      */
@@ -22,8 +22,8 @@ if (!function_exists('__i18nInit')) {
         $load = defined('CECR_I18N') ? CECR_I18N : true;
         if ($load) {
             $i18n = new i18n(
-                __DIR__.'/../../i18n/'.$locale.'.ini',
-                $cache === null ? __DIR__.'/../../tests/cache/' : $cache,
+                __DIR__ . '/../../i18n/' . $locale . '.ini',
+                $cache === null ? __DIR__ . '/../../tests/cache/' : $cache,
                 'en'
             );
             $i18n->setForcedLang($locale);
@@ -32,39 +32,53 @@ if (!function_exists('__i18nInit')) {
     }
 }
 
-if (! function_exists('__i18n')) {
+if (!function_exists('myEach')) {
+    /**
+     * Function to fix deprecated each() function
+     */
+    function myEach(&$arr)
+    {
+        $key    = key($arr);
+        $result = ($key === null) ? false : [$key, current($arr), 'key' => $key, 'value' => current($arr)];
+        next($arr);
+        return $result;
+    }
+}
+
+if (!function_exists('__i18n')) {
     /**
      * Returns a translation found in language files.
      * cecr = Comprobante Electronico Costa Rica
      * @since 1.0.0
-     * 
+     *
      * @param string $string String or key to use to find translation.
      * @param array  $args   Translation args.
-     * 
+     *
      * @return string
      */
     function __i18n($string, $args = [])
     {
         return function_exists('L')
-            ? L(mb_strtolower(trim(preg_replace(['/\s/', '/[\.\,\'\"\?\!\:\%\(\)\\\/]/'], ['_',''], $string))), $args)
+            ? L(mb_strtolower(trim(preg_replace(['/\s/', '/[\.\,\'\"\?\!\:\%\(\)\\\/]/'], ['_', ''], $string))), $args)
             : $string;
     }
 }
 
-if (! function_exists('__cecrDate')) {
+if (!function_exists('__cecrDate')) {
     /**
      * Returns date with valid format.
      * @since 1.0.0
-     * 
+     *
      * @param {mixed} $time Time or date value value to format to date.
-     * 
+     *
      * @return string
      */
     function __cecrDate($time = null)
     {
-        if ($time === null)
+        if ($time === null) {
             $time = time();
+        }
         $time = is_string($time) ? strtotime($time) : $time;
-        return date('Y-m-d', $time).'T'.date('H:i:s', $time).'Z';
+        return date('Y-m-d', $time) . 'T' . date('H:i:s', $time) . 'Z';
     }
 }
